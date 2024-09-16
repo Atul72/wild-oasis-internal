9747bfd241500c145318ef8d9e7c00011afdd71d
Atul72 <rai703537@gmail.com>
2024-09-17 00:22:02 +0530

    abstact react query

--- src/features/cabins/useEditCabin.js
+++ src/features/cabins/useEditCabin.js
@@ -3,19 +3,19 @@ import toast from "react-hot-toast";
 
 import { createEditCabin } from "../../services/apiCabins";
 
-export const useCreateCabin = (reset) => {
+export const useEditCabin = () => {
   const queryClient = useQueryClient();
-  const { mutate: createCabin, isLoading: isCreating } = useMutation({
-    mutationFn: createEditCabin,
+  const { mutate: editCabin, isLoading: isEditing } = useMutation({
+    mutationFn: ({ newCabinData, id }) => createEditCabin(newCabinData, id),
     onSuccess: () => {
-      toast.success("New cabin succesfully created.");
+      toast.success("Cabin sucessfully edited!");
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
-      reset();
+      //   reset();
     },
     onError: (err) => toast.error(err.message),
   });
 
-  return { createCabin, isCreating };
+  return { editCabin, isEditing };
 };
