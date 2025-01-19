194ebcc5668d0603f6930ab0e765a2e20759fc55
Atul72 <rai703537@gmail.com>
2025-01-19 23:30:03 +0530

    setup account password and avatar updation

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function signup({ fullName, email, password }) {
   let { data, error } = await supabase.auth.signUp({
@@ -39,4 +39,37 @@ export async function getCurrentUser() {
 export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) throw new Error(error.message);
+
+  if (!avatar) return data;
+
+  // 2. Update avatar in the supabase
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+  const filePath = `${fileName}.jpg`;
+
+  const { error: avatarError } = await supabase.storage
+    .from("avatars")
+    .upload(filePath, avatar);
+
+  if (avatarError) throw new Error(avatarError.message);
+
+  const { data: updatedData, error: updateError } =
+    await supabase.auth.updateUser({
+      data: {
+        avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      },
+    });
+
+  if (updateError) throw new Error(updateError.message);
+
+  return updatedData;
 }
